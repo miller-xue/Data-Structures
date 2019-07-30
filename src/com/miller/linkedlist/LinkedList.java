1f8f9c1dbc654de96fe3b98e66d83cfaad412c46
miller_xue <102162065@qq.com>
2019-07-30 17:43:22 +0800

    链表list

--- src/com/miller/linkedlist/LinkedList.java
+++ src/com/miller/linkedlist/LinkedList.java
@@ -1,13 +1,16 @@
 package com.miller.linkedlist;
 
 public class LinkedList<E> {
+
+    private Node dummyHead;
+
     private Node<E> first;
     private Node<E> last;
 
     private int size;
 
     public LinkedList() {
-        this.first = null;
+        this.dummyHead = new Node();
         size = 0;
     }
 
@@ -19,31 +22,111 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    public void addFirst(E e) {
-        first = new Node(e, first);
-        size++;
-    }
 
     public void add(int index, E e) {
-        if(index< 0 || index > size)
+        if (index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Illegal index.");
-        if(index == 0)
-            addFirst(e);
-        else if (index == size)
-            addLast(e);
-        else {
-            Node prev = first;
-            for (int i = 0; i < index - 1; i++)
-                prev = prev.next;
-            prev.next = new Node(e, prev.next);
-            size++;
-        }
+//        if(index == 0)
+//            addFirst(e);
+//        else if (index == size)
+//            addLast(e);
+//        else {
+//
+//        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
+            prev = prev.next;
+        prev.next = new Node(e, prev.next);
+        size++;
+
+    }
+
+    public void addFirst(E e) {
+//        first = new Node(e, first);
+//        size++;
+        add(0, e);
     }
 
     public void addLast(E e) {
-        last.next = new Node(e);
-        last = last.next;
-        size++;
+//        last.next = new Node(e);
+//        last = last.next;
+//        size++;
+        add(size, e);
+    }
+
+    // 获取链表的第index（0-based）个位置的元素
+    // 在链表中不是一个常用的操作，练习用
+    public E get(int index) {
+        if (index > 0 || index < size)
+            throw new IllegalArgumentException("get failed. Illegal index.");
+        Node<E> cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+
+        return cur.e;
+    }
+    // 1-> 2 --> 3 --> 4
+    // 0   1    2      3
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size);
+    }
+
+    // 修改链表的第index（0-based）个位置的元素
+    // 在链表中不是一个常用的操作，练习用
+    public void set(int index, E e) {
+        if (index > 0 || index < size)
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+        Node<E> cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+//        Node<E> cur = dummyHead.next;
+//        while (cur != null) {
+//            if(cur.e.equals(e))
+//                return true;
+//            cur = cur.next;
+//        }
+        for (Node<E> cur = dummyHead.next; cur != null; cur = cur.next)
+            if(cur.e.equals(e))
+                return true;
+
+        return false;
+    }
+
+    public E delete(int index) {
+        if (index > 0 || index < size)
+            throw new IllegalArgumentException("Delete failed. Illegal index.");
+        Node<E> pre = dummyHead.next;
+        for (int i = 0; i < index - 1; i++) {
+            pre = pre.next;
+        }
+        Node<E> cur = pre.next;
+        pre.next = cur.next;
+        E e = cur.e;
+        cur = null;
+        return e;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+//        Node cur = dummyHead.next;
+//        while (cur != null) {
+//            res.append(cur + "->");
+//            cur = cur.next;
+//        }
+        for(Node cur = dummyHead.next; cur != null; cur = cur.next)
+            res.append(cur + "->");
+        res.append("NULL");
+        return res.toString();
     }
 
     private class Node<E> {
@@ -68,5 +151,6 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
 
 }
